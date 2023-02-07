0c181769ecae2a811dc854fa7880ae88544f0123
Yong <uprisepro1@gmail.com>
2023-02-08 03:49:26 +0900

    [UPDATE] 6.1 pushAndRemoveUntil

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -20,6 +21,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
         _formKey.currentState!.save();
       }
     }
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => const InterestsScreen()),
+        (route) => false);
   }
 
   @override
